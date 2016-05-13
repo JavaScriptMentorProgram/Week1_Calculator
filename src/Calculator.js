714cc175ce9e77cf723584724afbd5c9b93cd496
Claire Liu <wenjing.liu09@foxmail.com>
2016-05-13 17:15:50 +0800

    Define Calculator class

---
+++ src/Calculator.js
@@ -0,0 +1,43 @@
+class Calculator{
+  constructor(){
+    this._result = 0;
+  }
+
+  get result(){
+    return this._result;
+  }
+
+  add(left, right){
+    return this._result = left+right;
+  }
+
+  sub(left, right){
+    return this._result = left-right;
+  }
+
+  multi(left, right){
+    return this._result = left*right;
+  }
+
+  div(left, right){
+    if(right === 0)
+      throw Error("Wrong input!");
+    else
+      return this._result = left/right;
+  }
+
+  mod(left, right){
+    return this._result = left%right;
+  }
+
+  equal(){
+    return this._result;
+  }
+
+  isNumber(param){
+    return typeof param === Number;
+  }
+}
+/*let calculator = new Calculator();
+
+module.exports = calculator;*/
